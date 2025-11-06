361b1f21d8063dc3db3ffe11023b7ac22a984c6f
Shylaja-Bingi <shylajabingi20@gmail.com>
2025-11-06 13:04:21 +0530

    Initial commit - frontend

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
-// client/src/config.js
-// config.js
-export const BASE_URL = "https://infohub-backend-cwwofn2q2-shylajabingi20-6931s-projects.vercel.app";
+
+export const BASE_URL = "https://infohub-production-178e.up.railway.app";
+
+
 
 
