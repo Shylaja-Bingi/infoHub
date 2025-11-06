b67ba96afe24604d19cb2a7d2eb7f00323492451
Shylaja-Bingi <shylajabingi20@gmail.com>
2025-11-06 12:28:19 +0530

    fix railway port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+
 
 // ---------- WEATHER API ----------
 const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
